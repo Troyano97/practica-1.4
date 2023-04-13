a1b83f9f44ea4792e6dcaef6430591b45b801bc9
Troyano97 <ivanferreyradaniel@gmail.com>
2023-04-12 21:10:07 -0300

    first commit

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+document.querySelector("body").innerHTML = `     
+<details>
+    <summary>Titulo referencia</summary><img src="https://t2.ea.ltmcdn.com/es/posts/7/4/3/como_ayudar_a_un_gatito_a_defecar_20347_orig.jpg" alt="gatito"><h />
+        <iframe src="https://www.mercadolibre.com.ar" frameborder="0"></iframe>
+        <p> Hola buenas tardes como estan?</p>
+        <a href="https://www.ebay.com/?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5337314645&customid=&toolid=10001&mkevt=1">LINK</a>
+    </details>
+    `;
